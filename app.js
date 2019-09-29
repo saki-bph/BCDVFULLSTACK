60a281e4966f2b37be947d774d62568849c7980f
sphaider <s9haider@uwaterloo.ca>
2019-09-29 18:39:50 -0400

    added types

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const search = document.getElementById("searchbutton");
 const sprite = document.getElementById("pokemon");
+const type = document.getElementById("type");
 search.addEventListener('click', function(event){
   event.preventDefault();
   findPokemon();
 });
 
-function getPokemonSprite(picture){
-  sprite.src = picture;
-  //pic.setAttribute("src",picture);
+function getPokemonInfo(pokemon){
+  sprite.src = pokemon.sprites.front_default;
+  console.log(pokemon.types[0].type.name);
+  let typeList = [];
+  for (var i = 0; i < pokemon.types.length; i++) {
+    typeList[i] = pokemon.types[i].type.name;
+  }
+  type.innerHTML = typeList;
 }
 function findPokemon(){
 
@@ -22,7 +28,7 @@ function findPokemon(){
       const newData = JSON.parse(data);
 
       console.log(newData);
-      getPokemonSprite(newData.sprites.front_default);
+      getPokemonInfo(newData);
 
     });
 }
