4fca2e781e2bb66adf93916d7aec5d9301fa46c6
sphaider <s9haider@uwaterloo.ca>
2019-09-29 20:53:41 -0400

    added weakness

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const search = document.getElementById("searchbutton");
 const sprite = document.getElementById("pokemon");
 const type = document.getElementById("type");
+const weakness = document.getElementById("weakness");
 search.addEventListener('click', function(event){
   event.preventDefault();
   findPokemon();
 });
+function findWeakness(pokemon){
+    let mono = pokemon.types[0].type.name;
+    fetch('https://pokeapi.co/api/v2/type/' + mono).then(function(response){
+      let ddata = response.json();
+      return ddata;
+    })
+    .then(function(ddata){
+      const adata = JSON.stringify(ddata);
+      const wdata = JSON.parse(adata);
+      let weaklist = wdata.damage_relations.double_damage_from;
+      getPokemonInfo(pokemon, weaklist);
+    });
+}
 
-function getPokemonInfo(pokemon){
+function getPokemonInfo(pokemon, weaklist){
+  console.log(weaklist);
   sprite.src = pokemon.sprites.front_default;
-  console.log(pokemon.types[0].type.name);
   let typeList = [];
   for (var i = 0; i < pokemon.types.length; i++) {
     typeList[i] = pokemon.types[i].type.name;
@@ -17,7 +31,13 @@ function getPokemonInfo(pokemon){
     typeList[1] = "";
   }
   type.innerHTML = typeList[0] + " " +typeList[1];
+  for (var i = 0; i < weaklist.length; i++) {
+    weakness.appendChild = weaklist[i].name;
+  }
+
 }
+
+
 function findPokemon(){
 
   const query = document.getElementById("searchbox").value;
@@ -29,9 +49,7 @@ function findPokemon(){
     .then(function(myJson){
       const data = JSON.stringify(myJson);
       const newData = JSON.parse(data);
-
-      console.log(newData);
-      getPokemonInfo(newData);
+      findWeakness(newData);
 
     });
 }
